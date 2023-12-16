5a676186ef1830fe5a169af9b436be4d7f66e9a9
Ивайло Пръмов <115698520+ivaylopr@users.noreply.github.com>
2023-12-16 11:36:29 +0200

    Associative Arrays solutions

---
+++ Associative Arrays/Lab&Exercise/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+SortedDictionary<int, int> sortedN = new SortedDictionary<int, int>();
+foreach (int n in numbers)
+{
+	if (!sortedN.ContainsKey(n))
+	{
+		sortedN.Add(n, 0);
+	}
+	sortedN[n]++;
+}
+foreach (var item in sortedN)
+{
+	Console.WriteLine($"{item.Key} -> {item.Value}");
+}
