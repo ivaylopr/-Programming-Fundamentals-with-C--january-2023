f9f124b62bf8d69f2248b3fabe78e5dfec91977a
Ивайло Пръмов <115698520+ivaylopr@users.noreply.github.com>
2023-12-16 11:34:49 +0200

    Add lab and exercise solution

---
+++ Object and Classes/Lab&Exercise/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+string[] words = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+for (int i = 0; i < words.Length; i++)
+{
+    Random random = new Random();
+    int randomIndex = random.Next(0,words.Length);
+    string currWord = words[i];
+    string toSwap = words[randomIndex];
+    words[i] = toSwap;
+    words[randomIndex] = currWord;
+}
+Console.WriteLine(string.Join(Environment.NewLine,words));
